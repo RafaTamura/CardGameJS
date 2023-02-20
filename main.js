1cfc2c8144b5d2fa3fd470cdbe34826b8ca8e9cc
Rafa Tamura <rafaelatamura@gmail.com>
2023-02-20 09:38:19 -0300

    Component CardFrontBack

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import BoardGame from "./src/objects/BoardGame/index.js";
 import './src/components/styles/settings/colors.css';
 import './src/components/styles/generic/reset.css';
 import './src/components/styles/elements/base.css';
 import ScoreBoard from "./src/objects/ScoreBoard/index.js";
+import CardFrontBack from './src/components/CardFrontBack';
 
 const $root = document.querySelector("#root");
 
@@ -10,7 +10,7 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML("beforeend",
 `
 ${ScoreBoard()}
-${BoardGame(6)},
+${CardFrontBack()}
 
 `
 
