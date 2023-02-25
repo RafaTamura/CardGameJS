fa67b6874e3bc268c5c21141e99327a749a3733e
Rafa Tamura <rafaelatamura@gmail.com>
2023-02-24 22:30:22 -0300

    Add click event

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML("beforeend",
 `
 ${ScoreBoard()}
-${BoardGame(2)}
+${BoardGame(6)}
 
 `
 
--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,16 +1,24 @@
 import CardGame from "../CardGame";
 import "./style.css";
 
+
 function CardFrontBack() {
+  window.cardFrontBack = {}
+  window.cardFrontBack.handleClick = (event) => {
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest('.card-front-back');
+   $cardFrontBack.classList.toggle('-active');
+  
+  }
   return /*html*/ `
-    <article class="card-front-back">
-      <div class="card -front">
-        ${CardGame()}
-      </div>
-      <div class="card -back">
-        ${CardGame("javascript", "Logo do JavaScript")}
-      </div>
-    </article>
+  <article class="card-front-back" onClick="window.cardFrontBack.handleClick(event)">
+  <div class="card -front">
+  ${CardGame()}
+  </div>
+  <div class="card -back">
+  ${CardGame("javascript", "Logo do JavaScript")}
+  </div>
+  </article>
   `;
 }
 
