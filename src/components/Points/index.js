fa11f6646b6ea26835c005d11f93a6331052889c
Rafa Tamura <rafaelatamura@gmail.com>
2023-02-25 23:02:10 -0300

    Add Points Blocks

---
+++ src/components/Points/index.js
@@ -0,0 +1,18 @@
+import "./style.css"
+
+function Points(){
+    return /*html*/ `
+    <div class="points">
+        <div class="points-mark">
+        </div>
+        <div class="points-mark2">
+        </div>
+        <div class="points-mark3">
+        </div>
+    
+    </div>
+    `
+
+}
+
+export default Points;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,14 @@
 import Player1 from "../../components/PlayerScore";
+import Points from "../../components/Points";
+import Versus from "../../components/Versus";
 import "./style.css"
 
 function ScoreBoard(){
     return /*html*/`
     <header class="score-board">
-    ${Player1("Player 1")}
-    ${Player1("Player 2")}
+    ${Player1("Player 1") + Points()}
+    ${Versus()}
+    ${Points() + Player1("Player 2")}
     </header>
     `
 
