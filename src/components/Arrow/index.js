484e75206e6b7967b568ec32282a8f93c367af30
Rafa Tamura <rafaelatamura@gmail.com>
2023-03-03 23:59:24 -0300

    Add component arrow down

---
+++ src/components/Arrow/index.js
@@ -0,0 +1,8 @@
+function Arrow(){
+return `
+<img src="/src/components/images/IconArrowDown.png" >
+`
+
+}
+
+export default Arrow;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,13 @@
 import Player1 from "../../components/PlayerScore";
 import Points from "../../components/Points";
 import Versus from "../../components/Versus";
+import Arrow from "../../components/Arrow";
 import "./style.css"
 
 function ScoreBoard(){
     return /*html*/`
     <header class="score-board">
+    ${Arrow()}
     ${Player1("Player 1")}
     ${Points(2)}
     ${Versus()}
